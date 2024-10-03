1f233bfcafe316bbd2527c64be76838429c11667
Rafaell Kruz <80438200+Rafaellkruz@users.noreply.github.com>
2024-10-03 11:26:41 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const cacheName = 'app-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/images/icon-192x192.png'
+];
+
+// Instalando o service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Ativando o service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptando as requisições de rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
